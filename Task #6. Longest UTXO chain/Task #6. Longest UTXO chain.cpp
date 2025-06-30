472e6aef1b8d602d5cd21496c7c663e979391e3d
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-30 12:22:03 +0300

    create struct Transactions

--- Task #6. Longest UTXO chain/Task #6. Longest UTXO chain.cpp
+++ Task #6. Longest UTXO chain/Task #6. Longest UTXO chain.cpp
@@ -1,4 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+
+struct Transaction {
+    std::string txID;
+    std::string input_txID;
+    int input_or_output_id;
+    std::vector<std::string> outputs;
+
+    Transaction(const std::string& txID_,
+        const std::string& input_txID_,
+        int input_or_output_id_,
+        const std::string out1,
+        const std::string out2)
+        : txID(txID_), input_txID(input_txID_), input_or_output_id(input_or_output_id_),
+        outputs{ out2, out2 } {}
+    std::string getInputID() const {
+        return input_txID + ":" + std::to_string(input_or_output_id);
+    }
+};
 
 int main() {
     std::cout << "Hello World!\n";
