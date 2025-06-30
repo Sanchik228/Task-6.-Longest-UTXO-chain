4d6582673c512ec32bb78a16a695112e128c4d9f
Sanchik228 <Oleksandra.Slastikhina@cs.khpi.edu.ua>
2025-06-30 13:18:49 +0300

    add CSV file and we can read from file

--- Task #6. Longest UTXO chain/Task #6. Longest UTXO chain.cpp
+++ Task #6. Longest UTXO chain/Task #6. Longest UTXO chain.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 struct Transaction {
     std::string txID;
@@ -14,12 +16,47 @@ struct Transaction {
         const std::string out1,
         const std::string out2)
         : txID(txID_), input_txID(input_txID_), input_or_output_id(input_or_output_id_),
-        outputs{ out2, out2 } {}
+        outputs{ out1, out2 } {}
     std::string getInputID() const {
         return input_txID + ":" + std::to_string(input_or_output_id);
     }
 };
 
+std::vector<Transaction> readFromCSV(const std::string& f) {
+    std::vector<Transaction> transactios;
+    std::ifstream file(f);
+
+    if (!file.is_open()) {
+        std::cerr << "File could not be opened: " << f << "\n";
+        return transactios;
+    }
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line)) {
+        if (line.empty()) continue;
+        
+        std::stringstream ss(line);
+        std::string txID, input_field, out1, out2;
+
+        if (!std::getline(ss, txID, ',')) continue;
+        if (!std::getline(ss, input_field, ',')) continue;
+        if (!std::getline(ss, out1, ',')) continue;
+        if (!std::getline(ss, out2, ',')) continue;
+
+        size_t colon_pos = input_field.find(':');
+        if (colon_pos == std::string::npos) continue;
+
+        std::string input_txID = input_field.substr(0, colon_pos);
+        int input_output_ID = std::stoi(input_field.substr(colon_pos + 1));
+
+        transactios.emplace_back(txID, input_txID, input_output_ID, out1, out2);
+    }
+    return transactios;
+}
+
 int main() {
-    std::cout << "Hello World!\n";
+    std::string filename = "transactions.csv";
+    std::vector<Transaction> txs = readFromCSV(filename);
+    std::cout << "Readed transactions: " << txs.size() << std::endl;
+    return 0;
 }
